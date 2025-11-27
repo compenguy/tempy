0932f170ca5b0ac208035695cff4ad8999738abd
William Page <williamtpage@gmail.com>
2025-11-27 11:32:06 -0800

    Update go.sh to separately handle esp.idf's monitor and menuconfig actions

--- go.sh
+++ go.sh
@@ -68,22 +68,28 @@ function activate_esp_matter() {
 }
 
 # Default values for command-line arguments
+ARG_CONFIG=0
 ARG_BUILD=0
 ARG_FLASH=0
 ARG_FULL_FLASH=0
+ARG_MONITOR=0
 
 # Argument parsing
 usage() {
 cat << EOF
-Usage: ${SCRIPT_NAME} [build-args] [flash-args]
+Usage: ${SCRIPT_NAME} [config-args] [build-args] [program-args] [debug-args]
 General Arguments:
   -h, --help		explanation of command line argument and environment
             		variable options
+Configuration Arguments:
+  -c, --menuconfig	invoke idf.py menuconfig
 Build Arguments:
   -b, --build		invoke idf.py to build the specified target
 Programming Arguments:
   -F, --full-flash	wipes out the entire flash and starts fresh (\`erase_flash\`)
   -f, --flash		write the built firmware to flash
+Debugging Arguments:
+  -m, --monitor		starts serial debug montoring of device via the USB serial interface (ESP_SERIAL=${ESP_SERIAL})
 EOF
 }
 
@@ -93,6 +99,10 @@ while [[ $# -gt 0 ]]; do
 			shift
 			ARG_BUILD=1
 			;;
+		-c|--menuconfig)
+			shift
+			ARG_CONFIG=1
+			;;
 		-f|--flash)
 			shift
 			ARG_FLASH=1
@@ -100,6 +110,10 @@ while [[ $# -gt 0 ]]; do
 		-F|--full-flash)
 			shift
 			ARG_FULL_FLASH=1
+			;;
+		-m|--monitor)
+			shift
+			ARG_MONITOR=1
 			;;
 		-h|--help)
 			usage
@@ -116,8 +130,10 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Task tracking variables
+CONFIG_ACTIVITY=0
 BUILD_ACTIVITY=0
 FLASH_ACTIVITY=0
+MONITOR_ACTIVITY=0
 
 activate_esp_idf
 activate_esp_matter
@@ -127,27 +143,52 @@ echo "Task(s) starting at $(date --rfc-3339=seconds)"
 echo "Configured build settings:"
 echo "    ESP target=${ESP_TARGET}"
 echo "    esp-idf version=${ESP_IDF_VERSION}"
+if [ "${ARG_CONFIG}" -ne 0 ]; then
+	log "Configuring project..."
+	(
+		cd "${SCRIPT_DIR}"
+		idf.py menuconfig
+	)
+	CONFIG_ACTIVITY=1
+fi
 if [ "${ARG_BUILD}" -ne 0 ]; then
 	log "Building project..."
-	idf.py build
+	(
+		cd "${SCRIPT_DIR}"
+		idf.py build
+	)
 	BUILD_ACTIVITY=1
 fi
 if [ "${ARG_FLASH}" -ne 0 ]; then
 	log "Flashing project..."
-	idf.py ${ESP_SERIAL_ARG} flash monitor
+	(
+		cd "${SCRIPT_DIR}"
+		idf.py ${ESP_SERIAL_ARG} flash
+	)
 	FLASH_ACTIVITY=1
 fi
 if [ "${ARG_FULL_FLASH}" -ne 0 ]; then
 	log "Full-storage flashing project..."
-	idf.py ${ESP_SERIAL_ARG} erase_flash
+	(
+		cd "${SCRIPT_DIR}"
+		idf.py ${ESP_SERIAL_ARG} erase_flash
+	)
 	FLASH_ACTIVITY=1
+fi
+if [ "${ARG_MONITOR}" -ne 0 ]; then
+	log "Flashing project..."
+	(
+		cd "${SCRIPT_DIR}"
+		idf.py ${ESP_SERIAL_ARG} monitor
+	)
+	MONITOR_ACTIVITY=1
 fi
 
 # Wrap-up
-if [[ ${BUILD_ACTIVITY} -eq 0 ]] && [[ ${FLASH_ACTIVITY} -eq 0 ]]; then
+if [[ ${CONFIG_ACTIVITY} -eq 0 ]] && [[ ${BUILD_ACTIVITY} -eq 0 ]] && [[ ${FLASH_ACTIVITY} -eq 0 ]] && [[ ${MONITOR_ACTIVITY} ]]; then
 	usage
 	echo ""
-	echo "Nothing to do! Did you mean to specify --build or --flash?"
+	echo "Nothing to do! Did you mean to specify --menuconfig, --build, --flash, or --monitor?"
 else
 	echo "Task(s) completed at $(date --rfc-3339=seconds)"
 	echo "Configured build settings:"
